189392961025591e6bac0f273c45da92bedc75f4
Yasin Khanabeigi <ykhanabe@c1r5s5.42lausanne.ch>
2024-06-18 11:58:19 +0200

    initial commit

---
+++ gitconnect.sh
@@ -0,0 +1,7 @@
+echo "# git-vogsphere.42lausanne.ch-vogsphere-intra-uuid-4599" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:Yasinkhanabeigi/git-vogsphere.42lausanne.ch-vogsphere-intra-uuid-4599.git
+git push -u origin main
